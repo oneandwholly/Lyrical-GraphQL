a910ef4dd5e5da5b43bf0880633cc16e6574d6c3
oneandwholly <oneandwholly@gmail.com>
2018-04-18 21:14:38 +0900

    get rid of mongolab uri

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = 'mongodb://myuser:8822@ds143907.mlab.com:43907/lyricaldb';
+const MONGO_URI = 'YOUR_MONGOLB_URI';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
